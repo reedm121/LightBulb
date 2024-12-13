0cb3bf600116de8d730cc5bb3033ab33d32c17a3
Reed Gantz <reedm121@gmail.com>
2024-12-13 12:00:40 -0500

    Initial Commit

---
+++ LightBulb/LightBulbApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightBulbApp.swift
+//  LightBulb
+//
+//  Created by Reed Gantz on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LightBulbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
